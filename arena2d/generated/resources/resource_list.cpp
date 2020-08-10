4068d77114870c559617a7edcbef4b94ff8eb49a
Joe <noahinde2014@gmail.com>
2020-08-10 01:39:16 +0000

    integrate arena2d simulator with ros.

--- arena2d/generated/resources/resource_list.cpp
+++
@@ -1,7 +0,0 @@
-// this file is generated automatically, DO NOT EDIT!
-#include "resource_list.h"
-
-const ResourceDescriptor RESOURCES_DESCRIPTOR[RESOURCES_NUMBER] = {
-	ResourceDescriptor(2, "data/fonts/Bitstream_Regular.ttf", -1, -1, ResourceOptions(ResourceOptions::Font(0))),
-	ResourceDescriptor(2, "data/fonts/Bitstream_Bold.ttf", -1, -1, ResourceOptions(ResourceOptions::Font(0)))
-};
